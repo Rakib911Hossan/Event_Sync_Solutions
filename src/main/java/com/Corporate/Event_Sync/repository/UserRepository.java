c6557203c4c2d8216388c01f1b7be21dfe255def
Rakib911Hossan <rakib911hossan@gmail.com>
2024-10-12 02:14:59 +0600

    Remove large JavaFX SDK

--- src/main/java/com/Corporate/Event_Sync/repository/UserRepository.java
+++ src/main/java/com/Corporate/Event_Sync/repository/UserRepository.java
@@ -4,5 +4,6 @@ import com.Corporate.Event_Sync.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+        User findByEmail(String email);
+    }
 
-}
